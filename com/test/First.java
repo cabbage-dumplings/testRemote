0cd47a36962b3333783f08dcde85d2130c4c31f8
cabbage-dumplings <384201386@qq.com>
2022-04-18 21:02:21 +0800

    第一次提交

---
+++ com/test/First.java
@@ -0,0 +1,40 @@
+package com.test;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class First {
+    static volatile int b = 0;
+    public static void main(String[] args) throws Exception {
+        test1();
+    }
+    public static void test1() throws Exception{
+        AtomicInteger a = new AtomicInteger(0);
+        Thread t1 = new Thread(() -> {
+            try {
+                //Thread.sleep(5000);
+                System.out.println();
+            } catch (Exception e) {
+                //System.out.println("被打断");
+                //System.out.println("被打断后"+Thread.currentThread().isInterrupted());
+                e.printStackTrace();
+            }
+        });
+        /*Thread t2 = new Thread(() -> {
+            try {
+                for (int i = 0; i < 10000; i++) {
+                    //b++;
+                    a.getAndIncrement();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });*/
+        t1.start();
+        System.out.println(t1.isInterrupted());
+        t1.interrupt();
+        for (int i = 0; i < 10000; i++) {
+            a.getAndIncrement();
+        }
+        System.out.println("主线程"+t1.isInterrupted());
+    }
+}
